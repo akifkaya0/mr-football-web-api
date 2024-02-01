272936da80b07d5bc04c5baa9ecd4ce247945741
akif kaya <m.akif5844@gmail.com>
2024-02-01 19:19:54 +0300

    file structure created

---
+++ src/main/java/com/mrf/mrfback/MrFootballBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.mrf.mrfback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrFootballBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrFootballBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrf/mrfback/MrFootballBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrf.mrfback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrFootballBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
